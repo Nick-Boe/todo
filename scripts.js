aad4c08eec71e1599c0dc475d21f91ca3fe8b195
Nick-Boe <71531343+Nick-Boe@users.noreply.github.com>
2020-09-24 20:09:41 -0700

    Improved JS.

--- scripts.js
+++ scripts.js
@@ -1,51 +1,61 @@
-var button = document.getElementsByClassName("enter")[0];
-var input = document.getElementsByClassName("userinput")[0];
-var ul = document.getElementById("allItems");
-var li = document.getElementsByTagName("li")[0];
-var listLength = ul.childElementCount;
-var active = li;
+const button = document.getElementsByClassName("enter")[0];
+let input = document.getElementsByClassName("userinput")[0];
+const ul = document.getElementById("allItems");
+let li = document.getElementsByTagName("li")[0];
+console.log(bg);
+let listLength = ul.childElementCount;
+let active = li;
+let removing;
 
-document.getElementById("allItems").addEventListener("click",function(e){
-    if (e.target && e.target.matches("li.item") && e.target.matches("li.active")) {
-        e.target.className+=' animate__backOutRight'
-        active.classList.remove("active")
-        setTimeout(() => {ul.removeChild(e.target);},750);
-    } else if (e.target && e.target.matches("li.item")) { 
-    active.classList.remove("active","animate__animated", "animate__pulse")
-    e.target.classList.remove("animate__backInLeft")
-    e.target.className+=' active animate__animated animate__pulse animate__infinite'
-    active = document.getElementsByClassName("active")[0];
+console.log(button);
+console.log(input);
+console.log(ul);
+console.log(li);
+console.log(listLength);
+console.log(active);
 
+document.getElementById("allItems").addEventListener("click", function (e) {
+    if (e.target && e.target == active && e.target != removing) {
+        console.log('success!');
+        e.target.className += ' animate__backOutRight removing';
+        removing = document.getElementsByClassName("removing")[0];
+        active.classList.remove("active");
+        setTimeout(() => {
+            active.classList.remove("removing")
+            ul.removeChild(e.target);
+        }, 750);
+
+    } else if (e.target.matches("li.item") && e.target != removing) {
+        console.log('success on activating!');
+        active.classList.remove("active", "animate__animated", "animate__pulse");
+        e.target.classList.remove("animate__backInLeft");
+        e.target.className += ' active animate__animated animate__pulse animate__infinite';
+        active = document.getElementsByClassName("active")[0];
     }
 });
 
-function inputLength(){
-    return input.value.length;
-}
+let inputLength = () => input.value.length;
 
-function addListItem(){
-    var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li)
-        li.className ='item list-group-item animate__animated animate__backInLeft';
-        input.value ="";
-        listLength = ul.childElementCount;
-        
-}
+const addListItem = () => {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    li.className = 'item list-group-item animate__animated animate__backInLeft';
+    input.value = "";
+    listLength = ul.childElementCount;
+};
 
-function addOnClick(){
-    if (inputLength()>0){
+const addOnClick = () => {
+    if (inputLength() > 0) {
         addListItem();
     }
-}
+};
 
-function addOnEnter(event){
-    if (inputLength()>0 && event.keyCode===13){
+const addOnEnter = (event) => {
+    if (inputLength() > 0 && event.keyCode === 13) {
         addListItem();
     }
-}
+};
 
 button.addEventListener("click", addOnClick);
-
-
 input.addEventListener("keypress", addOnEnter);
